ba80efb936c0c2489f8a42cdc8c8277222dd5c22
modestemax <modestemax@gmail.com>
2018-09-16 02:48:16 +0100

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
    * Application configuration section
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
    */
-  apps : [
+  apps: [
 
     // First application
     {
@@ -12,41 +12,41 @@ module.exports = {
       env: {
         COMMON_VARIABLE: 'true'
       },
-      env_production : {
+      env_production: {
         NODE_ENV: 'production'
       }
     },
 
-    // Second application
-    {
-      name      : 'WEB',
-      script    : 'web.js'
-    }
   ],
 
   /**
    * Deployment section
    * http://pm2.keymetrics.io/docs/usage/deployment/
    */
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      "--aws--": "",
+      "key1": "/home/max/.ssh/keysvirginia.pem",
+      host1: '54.210.121.117',
+      "--ovh--": "",
+      "key": "/home/max/.ssh/id_rsa",
+      host: '142.44.246.201',
+      ref: 'origin/remote',
+      repo: 'git@github.com:modestemax/mk2018.git',
+      path: '/home/ubuntu/mk2018',
+      'post-deploy': 'npm install && npm start'
     },
-    dev : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/development',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
-      env  : {
-        NODE_ENV: 'dev'
-      }
+    dev: {
+      // user: 'node',
+      // host: '212.83.163.1',
+      // ref: 'origin/master',
+      // repo: 'git@github.com:repo.git',
+      // path: '/var/www/development',
+      // 'post-deploy': 'npm run install && pm2 reload ecosystem.config.js --env dev',
+      // env: {
+      //   NODE_ENV: 'dev'
+      // }
     }
   }
 };
